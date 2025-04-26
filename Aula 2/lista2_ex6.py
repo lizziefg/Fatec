96e368b50ca5501b2063566e6b9be03f2509c02e
lizziefg <elisa.efg@gmail.com>
2025-04-26 10:51:24 -0300

    Create lista2_ex6.py
    Calculando a média

---
+++ Aula 2/lista2_ex6.py
@@ -0,0 +1,14 @@
+'''
+Exercício 6: Calculando a média
+Crie três variáveis chamadas nota1, nota 2 e nota 3 e atribua valores numéricos a elas.
+Calcule a média dessas notas e armazene o resultado em media.
+Por fim, exiba a seguinte mensagem formatada:
+"A média das notas [nota1], [nota2] e [nota3] é [media]."
+'''
+
+nota1 = 6
+nota2 = 7
+nota3 = 9
+#As operações matemáticas obedecem à regra PEMDAS
+media = (nota1 + nota2 + nota3)/3
+print(f"A média das notas {nota1}, {nota2} e {nota3} é {media}.")
