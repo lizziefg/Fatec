bb988e747e4671eaabfd381770a2e0093f7e79a7
lizziefg <elisa.efg@gmail.com>
2025-04-26 10:54:29 -0300

    Create lista2_ex9.py
    Convertendo Celsius para Fahrenheit

---
+++ Aula 2/lista2_ex9.py
@@ -0,0 +1,11 @@
+'''
+Exercício 9: Convertendo Celsius para Fahrenheit
+Crie uma variável celsius e atribua um valor numérico a ela.
+Converta esse valor para Fahrenheit usando a fórmula:
+fahrenheit = (celsius * 9/5) + 32
+Exiba o resultado formatado:
+"A temperatura de [celsius]ºC corresponde a [fahrenheit]ºF."
+'''
+celsius = 38.0
+fahrenheit = (celsius * 9/5) + 32
+print(f"A temperatura de {celsius}ºC corresponde a {fahrenheit}ºF.")
