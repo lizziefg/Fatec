97dbc14bcc1343ade894b42417926a218df858db
lizziefg <elisa.efg@gmail.com>
2025-04-26 11:06:42 -0300

    Create lista3_ex14.py
    Transformação da nota (0 a 10) em conceito (A a E)

---
+++ Aula 3/lista3_ex14.py
@@ -0,0 +1,12 @@
+nota = int(input('Informe a nota de 0 a 10: '))
+
+if 9 <= nota <= 10:
+    print('A')
+elif 7 <= nota < 9:
+    print('B')
+elif 5 <= nota < 7:
+    print('C')
+elif 3 <= nota < 5:
+    print('D')
+elif 0 <= nota < 3:
+    print('E')
