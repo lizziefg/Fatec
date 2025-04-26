78d2105752f170a7b0f46472d2990f8a32804ad1
lizziefg <elisa.efg@gmail.com>
2025-04-26 10:48:02 -0300

    Create lista2_ex3.py
    Concatenando strings

---
+++ Aula 2/lista2_ex3.py
@@ -0,0 +1,11 @@
+'''
+Exercício 3: Concatenando strings
+Peça ao usuário que digite seu primeiro nome e soborenome separadamente.
+Armazene as entradas em duas variáveis chamadas primeiro_nome e sobrenome.
+Em seguida, concatene essas duas variáveis e exiba uma mensagem de boas-vindas:
+"Olá, [primeiro_nome] [sobrenome]! Seja bem-vindo(a)!"
+'''
+
+primeiro_nome = input('Digite seu primeiro nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+print("Olá, " + primeiro_nome + " " + sobrenome + "! Seja bem vindo(a)!")
