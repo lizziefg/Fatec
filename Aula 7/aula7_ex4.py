0ea2736ec63f6fa2fd55c0999a0b5dc1f62e4948
lizziefg <elisa.efg@gmail.com>
2025-06-27 08:46:12 -0300

    Create aula7_ex4.py
    Acessando elementos da tupla pelo índice

---
+++ Aula 7/aula7_ex4.py
@@ -0,0 +1,9 @@
+'''Exercício 4 – Acesso por índice
+Dada a tupla com os dias da semana:
+Imprima o primeiro, o terceiro e o último dia da semana.'''
+
+dias = ('segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado', 'domingo')
+
+print(dias[0])
+print(dias[2])
+print(dias[6])
