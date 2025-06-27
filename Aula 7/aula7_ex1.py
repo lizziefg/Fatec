e83997c6d31319bebe79195083099bc7ee9cfd28
lizziefg <elisa.efg@gmail.com>
2025-06-27 08:45:02 -0300

    Create aula7_ex1.py
    Intersecção de conjuntos

---
+++ Aula 7/aula7_ex1.py
@@ -0,0 +1,8 @@
+'''Exercício 1 - Itens em comum
+Dado dois conjuntos (sets) representando os produtos comprados por duas pessoas no supermercado,
+Quais produtos João e Maria compraram em comum?'''
+
+joao = {"arroz", "feijão", "macarrão", "leite"}
+maria = {"leite", "café", "açúcar", "arroz"}
+
+print(joao.intersection(maria))
